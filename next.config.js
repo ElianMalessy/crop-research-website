469945642d4336ee0fbbfbad5339d2d7725fcea3
Elian Malessy <82927551+ElianMalessy@users.noreply.github.com>
2022-01-13 01:45:57 -0800

    design changes

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
 
 const nextConfig = {
-  webpack(config) {
-    config.infrastructureLogging = { debug: /PackFileCache/ };
-    return config;
-  },
   images: {
     disableStaticImages: true
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,15 @@ import {
   Grid,
   GridItem,
   useColorMode,
-  SimpleGrid
+  SimpleGrid,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark
 } from '@chakra-ui/react';
 import { ChevronDownIcon, SunIcon, MoonIcon } from '@chakra-ui/icons';
+import DatePicker from '../components/data-picker';
 
 export const ClickContext = createContext();
 export const CountryContext = createContext();
@@ -25,6 +31,7 @@ export default function Home() {
   const [crop, setCrop] = useState();
   const [clicked, setClicked] = useState();
   const [enhance, setEnhance] = useState();
+  const [startDate, setStartDate] = useState(new Date());
 
   return (
     <Flex w='100%' h='100%' style={{ padding: '1rem' }}>
@@ -32,67 +39,77 @@ export default function Home() {
         <title>Africa Crop Data Collection and Research Map</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Grid templateColumns='repeat(20, 1fr)' gap={4} w='100%'>
-        <GridItem w='100%'>
-          <Button onClick={toggleColorMode}> {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}</Button>
-        </GridItem>
-        <GridItem w='100%' colSpan={10}>
+      <Grid templateColumns='repeat(36, 1fr)' gap={2} w='100%'>
+        <GridItem w='100%' colSpan={20}>
+          <Grid templateColumns='repeat(12, 1fr)' gap={2} w='100%' zIndex='10'>
+            <GridItem w='100%'>
+              <Button onClick={toggleColorMode}> {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}</Button>
+            </GridItem>
+            <GridItem w='100%' colSpan={11}>
+              <SimpleGrid columns={5} spacing={2}>
+                <Menu>
+                  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                    Date
+                  </MenuButton>
+                  <MenuList>
+                    <DatePicker selectedDate={startDate} onChange={(date) => setStartDate(date)} isClearable={false} />
+                  </MenuList>
+                </Menu>
+                <Menu>
+                  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                    Countries
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem
+                      onClick={() => {
+                        setCountry('Nigeria');
+                        setClicked(true);
+                      }}
+                    >
+                      Nigeria
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        setCountry('Ethiopia');
+                        setClicked(true);
+                      }}
+                    >
+                      Ethiopia
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        setCountry('Kenya');
+                        setClicked(true);
+                      }}
+                    >
+                      Kenya
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+                <Menu>
+                  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                    Crops
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem onClick={() => setCrop('Maize')}>Maize</MenuItem>
+                    <MenuItem onClick={() => setCrop('Cowpea')}>Cowpea</MenuItem>
+                    <MenuItem onClick={() => setCrop('Cassava')}>Cassava</MenuItem>
+                    <MenuItem onClick={() => setCrop('Rice')}>Rice</MenuItem>
+                  </MenuList>
+                </Menu>
+                <Button onClick={() => setEnhance(enhance === true ? false : true)}>Toggle Enhance</Button>
+              </SimpleGrid>
+            </GridItem>
+          </Grid>
+
           <CountryContext.Provider value={country}>
             <ClickContext.Provider value={{ clicked, setClicked }}>
               <Map enhance={enhance} />
             </ClickContext.Provider>
           </CountryContext.Provider>
         </GridItem>
-        <GridItem w='100%' colSpan={1}>
-          <Button onClick={() => setEnhance(enhance === true ? false : true)}>Toggle Enhance</Button>
-        </GridItem>
-        <GridItem w='100%' colSpan={4} />
-        <GridItem w='100%' colSpan={4}>
-          <SimpleGrid columns={2} spacing={2}>
-            <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                Countries
-              </MenuButton>
-              <MenuList>
-                <MenuItem
-                  onClick={() => {
-                    setCountry('Nigeria');
-                    setClicked(true);
-                  }}
-                >
-                  Nigeria
-                </MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    setCountry('Ethiopia');
-                    setClicked(true);
-                  }}
-                >
-                  Ethiopia
-                </MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    setCountry('Kenya');
-                    setClicked(true);
-                  }}
-                >
-                  Kenya
-                </MenuItem>
-              </MenuList>
-            </Menu>
-            <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                Crops
-              </MenuButton>
-              <MenuList>
-                <MenuItem onClick={() => setCrop('Maize')}>Maize</MenuItem>
-                <MenuItem onClick={() => setCrop('Cowpea')}>Cowpea</MenuItem>
-                <MenuItem onClick={() => setCrop('Cassava')}>Cassava</MenuItem>
-                <MenuItem onClick={() => setCrop('Rice')}>Rice</MenuItem>
-              </MenuList>
-            </Menu>
-          </SimpleGrid>
-        </GridItem>
+
+        <GridItem w='100%' />
       </Grid>
     </Flex>
   );
